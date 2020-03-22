e81256d0d1647b6f72b2b74c0cc91d30e87523f4
tango <sm91wilson@gmail.com>
2020-03-21 23:26:01 -0400

    fixed formatting

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function () {
     // gets the current date
     var today = new Date();
-    var day = ("0" + today.getDate()).slice(-2)+'-'+("0" + (today.getMonth()+1)).slice(-2)+'-'+today.getFullYear();
+    var day = ("0" + today.getDate()).slice(-2) + '-' + ("0" + (today.getMonth() + 1)).slice(-2) + '-' + today.getFullYear();
     // function for setting whether time block is past present or future
-    function updateTimeBlock(){
+    function updateTimeBlock() {
         // gets the current hour
         var dayHours = today.getHours();
         // function to run through every timeblock and compare id number to current hour of the day
-        $(".time-block").each(function(){
-        // var to get the number value of each timeblock
+        $(".time-block").each(function () {
+            // var to get the number value of each timeblock
             var timeBlock = parseInt($(this).attr("id"));
             // if statement to check f the timeblock is past, present or future
-            if(timeBlock > dayHours){
+            if (timeBlock > dayHours) {
                 $(this).addClass("future");
-            }else if(timeBlock === dayHours){
+            } else if (timeBlock === dayHours) {
                 $(this).addClass("present");
                 $(this).removeClass("past");
-            }else{
+            } else {
                 $(this).removeClass("future", "present");
                 $(this).addClass("past");
-            } 
+            }
         })
     };
-    
+
     // saving action to local storage
-    $(".saveBtn").on("click", function(e){
+    $(".saveBtn").on("click", function (e) {
         var button = e.target
         var objective = $(button).siblings(".description").val();
         var timeSlot = $(button).parent().attr("id");
@@ -45,7 +45,7 @@ $(document).ready(function(){
     // display current date
     $("#currentDay").text(day);
     // check for new hour every minute
-    setInterval(updateTimeBlock, 1000*60);
+    setInterval(updateTimeBlock, 1000 * 60);
     updateTimeBlock();
 })
 
